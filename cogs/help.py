4bae27393e2672948303cebc2ff5636fd5f40a5f
Jerry.py <64660183+Jerry-py@users.noreply.github.com>
2021-05-28 13:58:31 -0400

    Looks better

--- cogs/help.py
+++ cogs/help.py
@@ -9,9 +9,12 @@ class helpcommand(commands.Cog):
     @commands.command()
     async def help(self, ctx):
         embed = discord.Embed(title="Help", description="Commands", color=discord.Color.red())
-        embed.add_field(name="Level", value="Shows a member's rank\n Usage:\n rank [member]")
+        embed.add_field(name="Level", value="Shows a member's rank")
         embed.add_field(name="Leaderboard", value="Shows the top 10 in the leaderboard\n Usage:\n leaderboard")
         embed.add_field(name="Set", value="Sets a user's xp or level\n Usage:\n set [level/xp] [member] [amount]")
+        embed.add_field(name="Usage:", value="**rank [member]**")
+        embed.add_field(name="Usage:", value="**leaderboard**")
+        embed.add_field(name="Usage:", value="**set [level/xp] [member] [amount]**")
         embed.set_thumbnail(url=self.bot.user.avatar_url)
         await ctx.send(embed=embed)
         
@@ -19,5 +22,3 @@ class helpcommand(commands.Cog):
 
 def setup(bot):
     bot.add_cog(helpcommand(bot))
-
-

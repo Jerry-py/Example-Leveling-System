aa289d5437e63afb1f152b5e4a2119ce4a429c99
Jerry.py <64660183+Jerry-py@users.noreply.github.com>
2021-05-28 12:10:40 -0400

    Fix some error

--- cogs/help.py
+++ cogs/help.py
@@ -6,34 +6,16 @@ class helpcommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(invoke_without_command=True)
+    @commands.command()
     async def help(self, ctx):
         embed = discord.Embed(title="Help")
-        embed.add_field(name="Level", value="Shows a member's rank")
-        embed.add_field(name="Leaderboard", value="Shows the top 10 in the leaderboard")
-        embed.add_field(name="Set", value="Sets a user's xp or level")
+        embed.add_field(name="Level", value="Shows a member's rank\n Usage:\n rank [member]")
+        embed.add_field(name="Leaderboard", value="Shows the top 10 in the leaderboard\n Usage:\n leaderboard")
+        embed.add_field(name="Set", value="Sets a user's xp or level\n Usage:\n set [level/xp] [member] [amount]")
         embed.set_thumbnail(url=self.bot.user.avatar_url)
         await ctx.send(embed=embed)
         
-    @help.command()
-    async def set(self, ctx):
-        embed = discord.Embed(title="Set", description="Command to set the user's xp/level")
-        embed.add_field(name="Usage", value="set [xp/level] [member] [amount]")
-        await ctx.send(embed=embed)
-
-
-    @help.command()
-    async def level(self, ctx):
-        embed = discord.Embed(title="Level", description="Command to set the user's Stats")
-        embed.add_field(name="Usage", value="rank [member]")
-        await ctx.send(embed=embed)
-
-    @help.command()
-    async def leaderboard(self, ctx):
-        embed = discord.Embed(title="Set", description="Command to set the user's xp/level")
-        embed.add_field(name="Usage", value="leaderboard")
-        await ctx.send(embed=embed)
-
+ 
 
 def setup(bot):
     bot.add_cog(helpcommand(bot))
